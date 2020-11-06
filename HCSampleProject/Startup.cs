9406f0d520afcf81f8c7b809885ad07ab9380f86
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2020-11-06 09:37:30 +0100

    remove unused aspnet core stuff

--- HCSampleProject/Startup.cs
+++ HCSampleProject/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace HCSampleProject
 {
@@ -26,8 +18,6 @@ namespace HCSampleProject
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddControllers();
-      services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "HCSampleProject", Version = "v1" }); });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,17 +26,11 @@ namespace HCSampleProject
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HCSampleProject v1"));
       }
-
-      app.UseHttpsRedirection();
 
       app.UseRouting();
 
       app.UseAuthorization();
-
-      app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
   }
 }
